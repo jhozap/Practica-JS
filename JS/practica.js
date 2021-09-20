3f329455621b63ef35f94c60874ebba343e7b392
Jzapata <jhonny.z1022@gmail.com>
2021-09-20 18:49:23 -0500

    variables

---
+++ JS/practica.js
@@ -0,0 +1,42 @@
+// uso de variables
+// var nombre = "jhonny";
+// nombre = 0;
+// console.log(typeof(nombre));
+// console.log("mi nombre es: " + nombre);
+
+
+// let o var
+// myName = 'Chris';
+
+// function logName() {
+//   console.log(myName);
+// }
+
+// logName();
+
+// var myName;
+
+// tipos de variable
+
+// variables numericas
+// let edad = 20;
+
+// variables string/texto
+// let nombre = "Jhonny";
+
+// vaiables booleanas
+// let vivo = true;
+
+// variables array
+// let array = ['uno', 2, 'tres'];
+
+// variables de tipo objeto
+// let obj = {
+//     nombre: 'Jhonny',
+//     edad: 26,
+// };
+
+// declaracion de constantes
+// const edad = 26;
+
+// edad = 20;
