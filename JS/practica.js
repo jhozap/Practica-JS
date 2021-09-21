1a48b791495fc33f9bfa8f74a5376f9a9384c1eb
Jzapata <jhonny.z1022@gmail.com>
2021-09-20 20:53:44 -0500

    variables

--- JS/practica.js
+++ JS/practica.js
@@ -1,5 +1,8 @@
 // uso de variables
 // var nombre = "jhonny";
+
+// // console.log(nombre);
+
 // nombre = 0;
 // console.log(typeof(nombre));
 // console.log("mi nombre es: " + nombre);
@@ -14,38 +17,61 @@
 
 // logName();
 
-// var myName;
+// let myName;
 
 // tipos de variable
 
 // variables numericas
-// let edad = 20;
+let edad = 20;
+console.log(edad);
 
 // variables string/texto
-// let nombre = "Jhonny";
+let nombre = "Jhonny";
+console.log(nombre);
 
 // vaiables booleanas
-// let vivo = true;
+let vivo = true;
+console.log(vivo);
 
 // variables array
-// let array = ['uno', 2, 'tres'];
+let array = ['uno', 2, 'tres'];
+
+console.log(array);
+console.log(array[0]);
 
 // declararion regular
 // let frutas = new Array();
 // frutas[0] = "manzana";
 // frutas[1] = "pera";
 
+
+
 // declaracion condensada
 // let frutas = new Array("manzana", "pera");
 
 //declaracion literal
-// let frutas = ["manzana", "pera"];
+let frutas = ["manzana", "pera"];
+console.log(frutas);
+
+frutas.push("mango");
+
+console.log(frutas);
+
+frutas.pop();
+
+console.log(frutas);
+
 
 // variables de tipo objeto
-// let obj = {
-//     nombre: 'Jhonny',
-//     edad: 26,
-// };
+let obj = {
+    nombre: 'Jhonny',
+    edad: 26,
+    nacionalidad: 'Colombiano',
+};
+
+console.log(obj);
+console.log(obj.nombre);
+
 
 // declaracion de constantes
 // const edad = 26;
