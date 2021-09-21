012acf1f044640473c848c0f2c6b8a9f2a5f4c53
Jzapata <jhonny.z1022@gmail.com>
2021-09-20 19:19:17 -0500

    condiciones switch

---
+++ JS/practica.js
@@ -0,0 +1,61 @@
+let a = 2;
+let b = 2;
+
+if(a > b) {
+    console.log("a es mayor que b");
+}
+
+if(b > a) {
+    console.log("b es mayor que a");
+}
+
+if(a == b) {
+    console.log("a es igual que b");
+}
+
+
+let mes = 1
+switch(mes) {
+    case 1:
+        console.log("Enero");
+        break;
+    case 2: 
+        console.log("Febrero");
+        break;
+    case 3: 
+        console.log("Marzo");
+        break;
+    case 4: 
+        console.log("Abril");
+        break;
+    case 5: 
+        console.log("Mayo");
+        break;
+    case 6: 
+        console.log("Junio");
+        break;
+    case 7: 
+        console.log("Julio");
+        break;
+    case 8: 
+        console.log("Agosto");
+        break;
+    case 9: 
+        console.log("Septiembre");
+        break;
+    case 10: 
+        console.log("Octubre");
+        break;
+    case 11: 
+        console.log("Noviembre");
+        break;
+    case 12: 
+        console.log("Diciembre");
+        break;
+    case 13: 
+        console.log("Agosto");
+        break;
+    default:
+        console.log("opcion no soportada");
+
+}
