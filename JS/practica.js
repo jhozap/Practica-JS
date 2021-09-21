195661306b0f056983c022c65143a6cc8d645582
Jzapata <jhonny.z1022@gmail.com>
2021-09-20 19:29:55 -0500

    ciclos

---
+++ JS/practica.js
@@ -0,0 +1,19 @@
+let curso = ['SCRUM', 'GIT', 'HTML', 'CSS', 'JS'];
+
+for(let i = 0; i < curso.length; i++) {
+    console.log(curso[i]);
+}
+
+let n = curso.length;
+
+while(n <= 5) {
+    console.log(n);
+    n--;
+}
+
+
+do {
+    console.log(n);
+    n--;
+}
+while(n <= 5)
