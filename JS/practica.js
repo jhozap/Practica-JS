5051e8853ef31adcabe579a9b2330322bada10f2
Jzapata <jhonny.z1022@gmail.com>
2021-09-20 21:11:06 -0500

    funciones

--- JS/practica.js
+++ JS/practica.js
@@ -1,12 +1,12 @@
-function hola() {
+function hola() {    
     return "Hola Mundo";
 }
 
-function suma(a = 1, b = 2)
+function suma(a = 2, b = 3)
 {
     return a + b;    
 }
 
-console.log(hola);
+console.log(hola());
 console.log(suma());
 console.log(suma(1, 2));
